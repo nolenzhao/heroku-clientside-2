3a8d13c3fdf77fa1bc12284005df9738477eed30
nolenzhao <n37zhao@uwaterloo>
2022-06-22 13:19:45 -0700

    firsty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  
+  const [data, setData] = useState({});
+
+
+  useEffect( () =>{
+    fetch("https://heroku-api-test-2.herokuapp.com/gallery/1")
+    .then(raw => raw.json())
+    .then(result =>{
+      setData(result);
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      Up n' running
+      <div>
+      {data.alt_text}
+    </div>
+
+    <img src = {data.img_url}/>
     </div>
   );
 }
 
 export default App;
+ /*
+    <div>
+      {data.alt_text}
+    </div>
+
+    <img src = {data.img_url}/>
+    
+*/
